73541cb6425578cb1596c55624df6ce1369d3372
onlyhappiness <onlyhappiness96@gmail.com>
2025-03-04 23:12:34 +0900

    init: moaring start

---
+++ src/lib/widgets/layout/app-bar.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let title = '';
+</script>
+
+<header class="sticky top-0 z-10 border-b border-gray-100 bg-white shadow-md">
+	<div class="flex h-14 items-center justify-between px-4">
+		<h1 class="text-lg font-medium">로고</h1>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+
+	import AppBar from '$lib/widgets/layout/app-bar.svelte';
+	import BottomNavigation from '$lib/widgets/layout/bottom-navigation.svelte';
+</script>
+
+<div class="flex min-h-screen justify-center bg-gray-200">
+	<div class="flex min-h-screen w-full max-w-sm flex-col bg-white">
+		<AppBar />
+
+		<main class="flex-1 overflow-auto bg-white">
+			{@render children()}
+		</main>
+
+		<BottomNavigation />
+	</div>
+</div>
